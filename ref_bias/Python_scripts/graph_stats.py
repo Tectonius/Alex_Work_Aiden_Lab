b8f2e776091d26a1955130edbaba66d9c88cb3f7
Alexander Ellis <alexbrazil2001@gmail.com>
2024-12-19 18:28:42 -0600

    ref bias analysis with graphs run with my analysis pipeline try 2

--- ref_bias/Python_scripts/graph_stats.py
+++ ref_bias/Python_scripts/graph_stats.py
@@ -20,7 +20,6 @@ for key, stats in stats_dict.items():
     stats['std_devs'] = stats['std_devs'][len(stats['std_devs'])//2 - 75: len(stats['std_devs'])//2 + 75]
     stats['variances'] = stats['variances'][len(stats['variances'])//2 - 75: len(stats['variances'])//2 + 75]
     plt.figure(figsize=(15, 6))
-    print(f"x: {x}\n means: {stats['means']}")
     plt.plot(x, stats['means'], label='Mean')
     plt.title(f'{key} - Mean')
     plt.xlabel('Offsets')
