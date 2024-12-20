aa5a72bc3448b91ceec1da85f0e32c574204bdfa
Mystimorphix <mystimorphix@gmail.com>
2024-12-20 17:11:51 -0500

    condense into subplots/condense repetitive graph elements

--- ref_bias/Python_scripts/graph_stats.py
+++ ref_bias/Python_scripts/graph_stats.py
@@ -12,6 +12,8 @@ with open(stats_pkl_file, 'rb') as f:
 
 offsets = list(range(-150,151))
 
+num_subplots = 4
+fontsize = 15
 for key, stats in stats_dict.items():
     x = offsets
     x = x[len(x)//2 - 75: len(x)//2 + 75]
@@ -19,35 +21,26 @@ for key, stats in stats_dict.items():
     stats['medians'] = stats['medians'][len(stats['medians'])//2 - 75: len(stats['medians'])//2 + 75]
     stats['std_devs'] = stats['std_devs'][len(stats['std_devs'])//2 - 75: len(stats['std_devs'])//2 + 75]
     stats['variances'] = stats['variances'][len(stats['variances'])//2 - 75: len(stats['variances'])//2 + 75]
-    plt.figure(figsize=(15, 6))
-    plt.plot(x, stats['means'], label='Mean')
-    plt.title(f'{key} - Mean')
-    plt.xlabel('Offsets')
-    plt.ylabel('Values')
-    plt.legend()
-    plt.savefig(f'graphs/{key}_mean.png')
-    plt.close()
 
-    plt.plot(x, stats['medians'], label='Median')
-    plt.title(f'{key} - Median')
-    plt.xlabel('Offsets')
-    plt.ylabel('Values')
-    plt.legend()
-    plt.savefig(f'graphs/{key}_median.png')
-    plt.close()
 
-    plt.plot(x, stats['std_devs'], label='Std Dev')
-    plt.title(f'{key} - Std Dev')
-    plt.xlabel('Offsets')
-    plt.ylabel('Values')
-    plt.legend()
-    plt.savefig(f'graphs/{key}_std_dev.png')
-    plt.close()
+    fig, ax = plt.subplots(num_subplots, figsize=(15,6*num_subplots), gridspec_kw={'hspace': 0.5})
 
-    plt.plot(x, stats['variances'], label='Variance')
-    plt.title(f'{key} - Variance')
-    plt.xlabel('Offsets')
-    plt.ylabel('Values')
-    plt.legend()
-    plt.savefig(f'graphs/{key}_variance.png')
+    ax[0].plot(x, stats['means'], label='Mean')
+    ax[0].set_title(f'{key} - Mean', fontsize=fontsize)
+
+    ax[1].plot(x, stats['medians'], label='Median')
+    ax[1].set_title(f'{key} - Median', fontsize=fontsize)
+
+    ax[2].plot(x, stats['std_devs'], label='Std Dev')
+    ax[2].set_title(f'{key} - Std Dev', fontsize=fontsize)
+
+    ax[3].plot(x, stats['variances'], label='Variance')
+    ax[3].set_title(f'{key} - Variance', fontsize=fontsize)
+
+    for i in range(num_subplots):
+        ax[i].set_xlabel('Offsets', fontsize=fontsize)
+        ax[i].set_ylabel('Values', fontsize=fontsize)
+        ax[i].legend(fontsize=fontsize)
+
+    fig.savefig(f'graphs/{key}_stats.png')
     plt.close()
